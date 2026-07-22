c5c6b25cb07cc40ca1a232f786d4e9023daeb477
Manoj Kumar Gangula <mkgangula1@gmail.com>
2026-07-22 23:56:51 +0530

    Add Register implementation

--- src/main/java/io/github/manojkumar_gangula/authentication_service/dto/request/RegisterRequest.java
+++ src/main/java/io/github/manojkumar_gangula/authentication_service/dto/request/RegisterRequest.java
@@ -1,12 +1,20 @@
 package io.github.manojkumar_gangula.authentication_service.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RegisterRequest {
+    @NotBlank
     private String username;
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 }
